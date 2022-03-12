495279760bc9a5b476752a8e063e7cc8fb8b2223
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-12 16:29:19 +0900

    Archive Todo App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import TodoApp from "./TodoApp";
 
 
 class App extends React.Component {
   render() {
     return (
-      <TodoApp />
+      <div>hoge</div>
     )
   }
 }
