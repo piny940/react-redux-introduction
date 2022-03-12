7bb2cbb80fc4c074defcf5f39d967d304ce09864
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-12 21:49:49 +0900

    Archive redux-todo-app

---
+++ src/redux-todo-app/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reportWebVitals from '../reportWebVitals';
+import tasksReducer from './reducers/tasks';
+import { TodoApp } from './components/tasks';
+
+
+const store = createStore(tasksReducer);
+
+const renderApp = () => {
+  ReactDOM.render(
+    <TodoApp store={store} />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(renderApp);
+renderApp();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
